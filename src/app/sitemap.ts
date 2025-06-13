bfae24e02996f7afbb1cfc4140c65fcbbf5d0cf5
Prince chukwuemeka <162118418+pchukwuemeka424@users.noreply.github.com>
2025-06-13 20:40:19 +0100

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://acehubtechnologies.com'
+  const baseUrl = 'https://acehubtechnologiesltd.co.uk'
   
   return [
     {
